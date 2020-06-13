31fbbfb517c8d52abbb0d1a12828dc69e7a194a4
An Ng. Quang <ngquangan@gmail.com>
2020-06-13 10:53:19 +0700

    Add some routes to auth route

--- api/src/modules/auth/route/index.ts
+++ api/src/modules/auth/route/index.ts
@@ -3,22 +3,25 @@ import { Router, Application } from "https://deno.land/x/oak/mod.ts";
 const authRoute = (app: Application) => {
     const router = new Router();
     router
-      .get("/auth/login", (context) => {
+      .post("/auth/login", (context) => {
         context.response.body = "It's work!";
       })
-      .get("/auth/register", (context) => {
+      .post("/auth/register", (context) => {
         context.response.body = "It's work!";
       })
       .get("/auth/forgot-password", (context) => {
         context.response.body = "It's work!";
       })
-      .get("/auth/change-password", (context) => {
+      .post("/auth/change-password", (context) => {
         context.response.body = "It's work!";
       })
       .get("/auth/revoke-token", (context) => {
         context.response.body = "It's work!";
       })
       .get("/auth/refresh-token", (context) => {
+        context.response.body = "It's work!";
+      })
+      .get("/auth/force-logout", (context) => {
         context.response.body = "It's work!";
       })
       .get("/auth/user", (context) => {
