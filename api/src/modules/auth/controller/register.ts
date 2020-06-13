31d01e0926e9c50ac6b36c12ba09094341bd32d0
An Ng. Quang <ngquangan@gmail.com>
2020-06-13 17:10:07 +0700

    Save refresh token in cookie

--- api/src/modules/auth/controller/register.ts
+++ api/src/modules/auth/controller/register.ts
@@ -5,11 +5,17 @@ export const registerController = async (context: Context) => {
     try {
         const bodyResult = await context.request.body({ asReader: false });
         const bodyData = bodyResult?.value;
-        const { _id, accessToken } = await createAccountService(bodyData);
+        const { _id, accessToken, refreshToken } = await createAccountService(
+          bodyData
+        );
+
+        context.cookies.set("refresh_token", refreshToken, { httpOnly: true });
+
         const dataResponse = {
           _id,
           ...bodyData,
           access_token: accessToken,
+          refresh_token: refreshToken
         };
         delete dataResponse.password;
         context.response.body = dataResponse;
