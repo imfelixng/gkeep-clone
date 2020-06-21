59e473b020861959a68b0c91cdce2d91bfc0ff75
An Ng. Quang <ngquangan@gmail.com>
2020-06-21 18:30:02 +0700

    Fix type for Custom App

--- next-app/pages/_app.tsx
+++ next-app/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from 'next/app'
 import '../assets/style/index.scss'
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
 export default App;
